b36486e88b344ef0b25d20ce3b54f94c25265b64
Dominik Balga <balga.domo@gmail.com>
2023-10-10 20:28:17 +0200

    adjusting post_gen_project.py

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -5,4 +5,5 @@ PROJECT_REGEX = r"^[_a-zA.Z][-a-zA-Z0-9]+$"
 project_name = "{{cookiecutter.project_name}}"
 
 if not re.match(PROJECT_REGEX, project_name):
-    raise ValueError("%s is not a valid Python project name" %project_name)
+    print("%s is not a valid Python project name" %project_name)
+    sys.exit(1)
